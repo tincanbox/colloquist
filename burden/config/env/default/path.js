d48de078adb9202d42602a64b3822e087655c1d9
dev <matsui@psersonalagent.co.jp>
2019-07-26 14:45:07 +0900

    f

--- burden/config/env/default/path.js
+++ burden/config/env/default/path.js
@@ -1,5 +1,5 @@
 const path = require('path');
-let app = path.resolve([__dirname, '..', '..', '..'].join(path.sep));
+let app = path.resolve([__dirname, '..','..','..','..'].join(path.sep));
 module.exports = {
   app: app,
   log: [app, 'log'].join(path.sep),
