3baa5ae955041c0b50f1eef1d29efb634bffbc37
matooy <matsui@psersonalagent.co.jp>
2019-07-31 13:28:06 +0900

    adjusted scenario flow

--- burden/shelf/sample.js
+++ burden/shelf/sample.js
@@ -12,41 +12,41 @@ module.exports = class extends Story {
     ]);
 
     this.compose([
-      "sample",
+      "sample"
     ]);
   }
 
+
   async chapter_sample(){
 
-    await this.page.goto('https://news.google.com/?hl=en-US');
+    await this.page.goto('https://news.yahoo.com/');
+
+    /* Waits $P is injected.
+     * $P is injected (for all page) jQuery instance.
+     */
     await this.prepare();
 
-    /* $P is injected (for all page) jQuery instance.
+    /* Waits and finds target elements.
      */
     await FM.async.poll(async (rs) => {
       var l = await this.page.evaluate(() => {
-        return $P('c-wiz').length > 0;
+        return $P('.js-stream-content').length > 0;
       });
-      console.log(l);
       if(l) rs(true);
-    })
+    });
 
+    /* DOM manipulations via $P.
+     */
     var headlines = await this.page.evaluate(() => {
       var titles = [];
-      $P('c-wiz > div > div[jscontroller]').each((el) => {
+      $P('.js-stream-content').each((i, el) => {
         var wrap = $P(el);
-        titles.push(wrap.find('article h3').text());
+        titles.push(wrap.find('h3').text());
       });
       return titles;
     });
-    console.log(headlines);
 
-    await FM.async.sleep(10000);
-    await this.page.evaluate(() => {
-      $P('input[name="btnK"]').click();
-    });
-
-    await this.page.waitForNavigation();
+    this.remember('headlines', headlines);
   }
 
 }
--- run/app.js
+++ run/app.js
@@ -1,17 +1,29 @@
+#!/usr/local/bin/node
 const path = require('path');
 
+/* Very simple app with colloquist.
+ */
 (async () => {
 
+  /* Loads core class.
+   */
   const colloquist = require([__dirname, '..','lib','colloquist'].join(path.sep));
 
+  /* Instantiates colloquist with burden dir configuration.
+   */
   var c = new colloquist({
     // Set a path which includes burden dir.
-    burden: path.resolve(__dirname + path.sep + '..')
+    burden: path.resolve([__dirname, '..', 'burden'].join(path.sep))
   });
 
+  /* Start colloquist basics.
+   */
   await c.open({
-    foo: 2
+    more_arg: 123
   });
-  await c.recite('sample');
+
+  /* Observe command-line arguments.
+   */
+  await c.execute();
 
 })();
