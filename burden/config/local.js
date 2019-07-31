2f11d15ddfc5bf1f78b801d245a36c46df7f2ab8
matooy <matsui@psersonalagent.co.jp>
2019-07-31 15:28:08 +0900

    f

--- burden/config/local.js
+++ burden/config/local.js
@@ -1,16 +1,7 @@
 /* Local configurations.
  */
 
-const path = require('path');
-let app = path.resolve([__dirname, '..', '..'].join(path.sep));
-
 module.exports = {
   label: process.env.APPNAME || "YOURAPPNAME",
   stage: process.env.ENVIRONMENT || "devel",
-  path: {
-    app: app,
-    log: [app, 'log'].join(path.sep),
-    config: [app, 'burden', 'config'].join(path.sep),
-    shelf: [app, 'burden', 'shelf'].join(path.sep)
-  }
 }
