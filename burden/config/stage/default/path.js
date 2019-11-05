cc3189519e0a24db501afd6c8db5052820eb253a
matooy <matsui@psersonalagent.co.jp>
2019-11-05 13:39:13 +0900

    removed path.js and merged into ini.js

--- burden/config/stage/default/path.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-let app = path.resolve([__dirname, '..','..','..','..'].join(path.sep));
-module.exports = {
-  app: app,
-  log: [app, 'log'].join(path.sep),
-  config: [app, 'burden', 'config'].join(path.sep),
-  shelf: [app, 'burden', 'shelf'].join(path.sep)
-}
---
+++ burden/ini.js
@@ -0,0 +1,10 @@
+const path = require('path');
+let app = path.resolve([__dirname, '..'].join(path.sep));
+module.exports = {
+  path: {
+    app: app,
+    log: [app, 'log'].join(path.sep),
+    config: [app, 'burden', 'config'].join(path.sep),
+    shelf: [app, 'burden', 'shelf'].join(path.sep)
+  }
+}
