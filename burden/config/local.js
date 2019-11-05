5aad0e7b1691016a495580e8e10d72a9d109ab1f
matooy <matsui@psersonalagent.co.jp>
2019-11-05 18:52:38 +0900

    reverted puppeteer...

--- burden/config/local.js
+++ burden/config/local.js
@@ -2,12 +2,8 @@
  */
 
 module.exports = {
+
   label: process.env.APPNAME || "YOURAPPNAME",
   stage: process.env.ENVIRONMENT || "devel",
-
-  puppet: {
-    // chromium executable from `npm install -g chromium` on Ubuntu.
-    executable: "/usr/local/lib/node_modules/chromium/lib/chromium/chrome-linux/chrome"
-  }
 
 }
