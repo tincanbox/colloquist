29cef6b7ba66331058d8e7ad24a50dfb52bd4395
matooy <matsui@psersonalagent.co.jp>
2019-12-08 18:15:02 +0900

    fixed server and separated submodules:mail template

--- burden/config/stage/default/server.js
+++ burden/config/stage/default/server.js
@@ -3,24 +3,55 @@ module.exports = {
     port: 9000,
     prepare: async function(server){
 
-      server.router.get('*', async function(ctx){
-        console.log("Request URL:", ctx.request.url);
+      var block_list = [
+        "/favicon.ico"
+      ];
 
-        // Blocks stupid request.
-        var block = [
-          "/favicon.ico"
-        ];
-        if(block.indexOf(ctx.request.url) > -1){
-          return;
-        }
-
-        var t = ctx.request.url.replace(/^\//, "");
-        var c = await server.core.run(t);
-        ctx.body = c.scenario.passed;
+      var data = FM.ob.define({
+        post: {},
+        result: []
       });
 
-      server.engine.use(server.router.routes());
-      server.engine.use(server.router.allowedMethods());
+
+      async function load_view(req, param){
+        var t = req.url.split("?").shift().replace(/^\//, "");
+        t = "view/" + t + ".ejs";
+        try{
+          var view = await server.core.template.load(
+            t.split("/"), data(FM.ob.merge({}, req.query || {}, param))
+          );
+          return view;
+        }catch(e){
+          console.log(e);
+        }
+      }
+
+      server.router.all('*', function(req, res, next){
+        // Blocks stupid request.
+        if(block_list.indexOf(req.url) > -1){
+          console.log("Blocked: ", req.url);
+          return res.status(404).end();
+        }
+        next();
+      });
+
+      /* Really simple routing.
+       */
+      server.engine.get('*', async function(req, res){
+        var v = await load_view(req);
+        res.send(v);
+      });
+
+      server.engine.post('*', async function(req, res){
+        var t = req.url.replace(/^\//, "");
+        var c = await server.core.run(t);
+        var v = await load_view(req, {
+          post: req.body,
+          result: c.scenario.passed
+        });
+        res.send(v);
+      });
+
     }
   }
 };
