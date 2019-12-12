dc826dc2f91a415e14664b7553cf642b0bb2f4ac
matooy <matsui@psersonalagent.co.jp>
2019-12-12 14:53:13 +0900

    Changed ini.js format

--- burden/ini.js
+++ burden/ini.js
@@ -1,14 +1,15 @@
 const path = require('path');
-let app = path.resolve([__dirname, '..'].join(path.sep));
-module.exports = {
-  path: {
-    app: app,
-    log: [app, 'log'].join(path.sep),
-    config: [app, 'burden', 'config'].join(path.sep),
-    shelf: [app, 'burden', 'shelf'].join(path.sep),
-    story: [app, 'burden', 'shelf', 'story'].join(path.sep),
-    draft: [app, 'burden', 'shelf', 'draft'].join(path.sep),
-    schema: [app, 'burden', 'shelf', 'schema'].join(path.sep),
-    template: [app, 'burden', 'shelf', 'template'].join(path.sep),
-  }
-}
+
+let e = {path: {}};
+
+e.path.app = path.resolve([__dirname, '..'].join(path.sep)) + path.sep;
+e.path.log = e.path.app + 'log' + path.sep;
+e.path.burden = e.path.app + 'burden' + path.sep;
+e.path.config = e.path.burden + 'config' + path.sep;
+e.path.shelf = e.path.burden + 'shelf' + path.sep;
+e.path.story = e.path.shelf + 'story' + path.sep;
+e.path.draft = e.path.shelf + 'draft' + path.sep;
+e.path.schema = e.path.shelf + 'schema' + path.sep;
+e.path.template = e.path.shelf + 'template' + path.sep;
+
+module.exports = e;
