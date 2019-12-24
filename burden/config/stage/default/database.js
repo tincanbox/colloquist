c1428df025d244d61354ae6b688f484a699a77fd
matooy <matsui@psersonalagent.co.jp>
2019-12-24 11:27:12 +0900

    added sequelizer timestamp column config and fixed comments

--- burden/config/stage/default/database.js
+++ burden/config/stage/default/database.js
@@ -50,25 +50,41 @@ module.exports = {
     },
     factory: {
       option: {
-        // The name of the model. The model will be stored in `sequelize.models` under this name.
-        // This defaults to class name i.e. Bar in this case. This will control name of auto-generated
+        // The name of the model. The model will be stored in `sequelize.models`
+        // under this name.
+        // This defaults to class name i.e. Bar in this case.
+        // This will control name of auto-generated
         // don't add the timestamp attributes (updatedAt, createdAt)
         timestamps: true,
+
         // don't delete database entries but set the newly added attribute deletedAt
         // to the current date (when deletion was done). paranoid will only work if
         // timestamps are enabled
         paranoid: true,
+
         // Will automatically set field option for all attributes to snake cased name.
         // Does not override attribute with field option already defined
         underscored: true,
+
         // disable the modification of table names; By default, sequelize will automatically
         // transform all passed model names (first parameter of define) into plural.
         // if you don't want that, set the following
         freezeTableName: true,
-        // Enable optimistic locking.  When enabled, sequelize will add a version count attribute
-        // to the model and throw an OptimisticLockingError error when stale instances are saved.
+
+        // Enable optimistic locking.  When enabled, sequelize will add a
+        // version count attribute
+        // to the model and throw an OptimisticLockingError error
+        // when stale instances are saved.
         // Set to true or a string with the attribute name you want to use to enable.
         version: true,
+
+        // If you want sequelize to handle timestamps,
+        // but only want some of them, or want your timestamps to be called something else,
+        // you can override each column individually:
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at',
+
       },
       /*
       init: function(){
